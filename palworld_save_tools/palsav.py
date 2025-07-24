045156d658f9ecc3cddba7dda24915ba5b5f990f
I_aM_O <orod41285@gmail.com>
2025-07-23 21:11:20 -0400

    Add logging config to entry points

--- palworld_save_tools/palsav.py
+++ palworld_save_tools/palsav.py
@@ -1,14 +1,34 @@
+import sys
 from palworld_save_tools.compressor import Compressor
 from palworld_save_tools.compressor.oozlib import OozLib
 from palworld_save_tools.compressor.zlib import Zlib
 from palworld_save_tools.compressor.enums import SaveType
+
+from loguru import logger
 
 compressor = Compressor()
 oozlib = OozLib()
 z_lib = Zlib()
 
 
-def decompress_sav_to_gvas(data: bytes, zlib: bool = False) -> tuple[bytes, int]:
+def configure_logging(debug: bool = False):
+    if debug:
+        logger.remove()
+        logger.add(
+            sys.stdout,
+            colorize=True,
+            level="DEBUG",
+            format="{time:YYYY-MM-DD HH:mm:ss} | <level>{level: <8}</level> | <cyan>{name}</cyan>:<blue>{function}</blue>:{line} 🡆 {message}",
+        )
+    else:
+        logger.remove()
+        logger.add(
+            sys.stdout, format="<level>{level}</level> 🡆 {message}", level="INFO"
+        )
+
+
+def decompress_sav_to_gvas(data: bytes, debug: bool = False) -> tuple[bytes, int]:
+    configure_logging(debug)
     format = compressor.check_sav_format(data)
 
     if format is None:
@@ -23,7 +43,8 @@ def decompress_sav_to_gvas(data: bytes, zlib: bool = False) -> tuple[bytes, int]
             raise Exception("Unknown save format")
 
 
-def compress_gvas_to_sav(data: bytes, save_type: int, zlib: bool = False) -> bytes:
+def compress_gvas_to_sav(data: bytes, save_type: int, debug: bool = False) -> bytes:
+    configure_logging(debug)
     format = compressor.check_savtype_format(save_type)
 
     if format is None:
